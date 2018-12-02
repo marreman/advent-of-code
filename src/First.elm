6ea7cb5d5dc599c487cd6f8f480c3e57ea536b21
Martin Liberg <martin.liberg@drivekitt.com>
2018-12-02 11:29:35 +0100

    Take on part two

--- src/First.elm
+++ src/First.elm
@@ -19,13 +19,15 @@ main =
 
 
 type alias Model =
-    { result : Int
+    { sum : Int
+    , firstFreqReachedTwice : Maybe Int
     }
 
 
 init : Model
 init =
-    { result = 0
+    { sum = 0
+    , firstFreqReachedTwice = Nothing
     }
 
 
@@ -41,13 +43,61 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         NewInput input ->
+            let
+                values =
+                    parseRawInput input
+
+                inter1 =
+                    toIntermediarySums values
+
+                inter2 =
+                    toIntermediarySums inter1
+
+                inter3 =
+                    toIntermediarySums inter2
+            in
             { model
-                | result =
-                    input
-                        |> String.split "\n"
-                        |> List.map (String.toInt >> Maybe.withDefault 0)
-                        |> List.sum
+                | sum = List.sum values
+                , firstFreqReachedTwice =
+                    inter3
+                        |> toIntermediarySums
+                        |> Debug.log "sums"
+                        |> findDuplicates
+                        |> Debug.log "first"
+                        |> Just
+                        |> always Nothing
             }
+
+
+parseRawInput : String -> List Int
+parseRawInput =
+    String.split "\n"
+        >> List.map (String.toInt >> Maybe.withDefault 0)
+
+
+toIntermediarySums =
+    let
+        lastFreq =
+            List.head >> Maybe.withDefault 0
+
+        handle freqChange frequencies =
+            freqChange + lastFreq frequencies :: frequencies
+    in
+    List.foldl handle []
+        >> List.reverse
+
+
+findDuplicates freqs =
+    freqs
+        |> List.indexedMap
+            (\i freq ->
+                ( freq
+                , freqs
+                    |> List.indexedMap (\j freq2 -> i /= j && freq == freq2)
+                    |> List.any ((==) True)
+                )
+            )
+        |> List.filter (Tuple.second >> (==) True)
 
 
 
@@ -63,7 +113,11 @@ view model =
             , textarea [ onInput NewInput ] []
             ]
         , fieldset []
-            [ legend [] [ text "Result" ]
-            , span [] [ text (String.fromInt model.result) ]
+            [ legend [] [ text "Sum of input (first half of challenge)" ]
+            , span [] [ text (String.fromInt model.sum) ]
+            ]
+        , fieldset []
+            [ legend [] [ text "First frequency reached twice (second half of challenge)" ]
+            , span [] [ text (Debug.toString model.firstFreqReachedTwice) ]
             ]
         ]
