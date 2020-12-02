a3962201c5a22a3707a16fe960a00614f0f259e0
Martin Liberg <martin.liberg@m.co>
2020-12-02 22:18:28 +0100

    Clean up day 2 some more

--- 2020/clojure/src/aoc/day_02.clj
+++ 2020/clojure/src/aoc/day_02.clj
@@ -1,5 +1,5 @@
 (ns aoc.day-02
-  (:require [aoc.util :as util]))
+  (:require [aoc.util :refer [lines str->int xor]]))
 
 (def sample
   ["1-3 a: abcde"
@@ -7,29 +7,29 @@
    "2-9 c: ccccccccc"])
 
 (def input
-  (util/lines (slurp "../input/02")))
+  (lines (slurp "../input/02")))
 
-(defn- parse-policy [s]
+(defn parse-policy [s]
   (let [regex #"^(\d+)-(\d+) (\w): (\w+)$"
         [_ n1 n2 char phrase] (re-find regex s)]
-    [(util/str->int n1)
-     (util/str->int n2)
+    [(str->int n1)
+     (str->int n2)
      (first char)
      phrase]))
 
-(defn valid-password? [[n1 n2 char phrase]]
-  (let [char-count (count (filter (partial = char) phrase))]
-    (<= n1 char-count n2)))
+(defn count-valid-phrases [predicate]
+  (count (filter (comp predicate parse-policy) input)))
 
 (defn part-1 []
-  (count (filter (comp valid-password? parse-policy) input)))
-
-(defn valid-password-2? [[n1 n2 char phrase]]
-  (util/xor (= char (get phrase (dec n1)))
-            (= char (get phrase (dec n2)))))
+  (count-valid-phrases
+    (fn [[n1 n2 char phrase]]
+      (<= n1 (count (filter (partial = char) phrase)) n2))))
 
 (defn part-2 []
-  (count (filter (comp valid-password-2? parse-policy) input)))
+  (count-valid-phrases
+    (fn [[n1 n2 char phrase]]
+      (xor (= char (get phrase (dec n1)))
+           (= char (get phrase (dec n2)))))))
 
 (part-1) ;; 572
 (part-2) ;; 306
