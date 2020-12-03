9ddc41e9e051b7759d61629dff12e5488be93f0d
Martin Liberg <martin.liberg@m.co>
2020-12-03 20:26:50 +0100

    Day 3 part 2

--- 2020/clojure/src/aoc/day_03.clj
+++ 2020/clojure/src/aoc/day_03.clj
@@ -17,13 +17,20 @@
 (def input
   (lines (slurp "../input/03")))
 
-(defn part-1 [m]
+(defn part-1 [m [dx dy]]
   (loop [x 0, y 0, r 0]
     (if-let [row (get m y)]
       (case (nth (cycle row) x)
-        \. (recur (+ x 3) (+ y 1) r)
-        \# (recur (+ x 3) (+ y 1) (+ r 1))
-        nil (recur x (+ y 1) r))
+        \. (recur (+ x dx) (+ y dy) r)
+        \# (recur (+ x dx) (+ y dy) (+ r 1)))
       r)))
 
-(part-1 input)
+(defn part-2 [m]
+  (* (part-1 m [1 1])
+     (part-1 m [3 1])
+     (part-1 m [5 1])
+     (part-1 m [7 1])
+     (part-1 m [1 2])))
+
+(part-1 input [3 1]) ;; 159
+(part-2 input) ;; 6419669520
