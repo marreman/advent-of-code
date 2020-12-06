086687929503b91d1c0f507d8351b8521df53eb4
Martin Liberg <martin.liberg@m.co>
2020-12-06 11:02:04 +0100

    Day 6 part 2

--- 2020/clojure/src/aoc/day_06.clj
+++ 2020/clojure/src/aoc/day_06.clj
@@ -1,5 +1,6 @@
 (ns aoc.day-05
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def sample "abc
 
@@ -22,5 +23,14 @@
 (defn part-1 [s]
   (apply + (map #(count (set (re-seq #"[a-z]" %))) (str/split s #"\n\n"))))
 
+(defn all-answers [s]
+  (map #(str/split % #"\n") (str/split s #"\n\n")))
+
+(defn part-2 [s]
+  (apply + (map #(count (apply set/intersection (map set %))) (all-answers s))))
+
 (part-1 sample) ;; 11
 (part-1 input) ;; 6748
+
+;; (part-2 sample) ;; Sample doesn't work with part-2 because of white space in the string
+(part-2 input) ;; 3445
